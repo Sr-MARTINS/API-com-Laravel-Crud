59637d65108bdaddd424da636cb2f9715edb4c3d
Martins <victorcostamartins0@gmail.com>
2025-05-26 00:53:47 -0300

    API metodo delete

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
+use Mockery\Expectation;
 
 class UserController extends Controller
 {
@@ -20,8 +21,8 @@ class UserController extends Controller
 
     public function index() : JsonResponse
     {
-        //Recupera os usuarios do banco de dados, ordenado pelo id (podemos ursar uma ' , ' e passar um 'DESC' da vida, para ser ordenado de forma decresente ) o paginados
-        $usuarios = User::orderBy('id')->paginate(2);
+        //Recupera os usuarios do banco de dados, ordenado pelo id (podemos ursar uma ' , ' e passar um 'DESC' da vida, para ser ordenado de forma decresente ) o paginados  " ->paginate(2) "
+        $usuarios = User::orderBy('id')->get();
 
         // Retorna os usuarios recuperados como uma resposta JSON 
         return response()->json([
@@ -40,7 +41,8 @@ class UserController extends Controller
 
 
     /**
-     * Criar novo usuario com os dados fornecidos na requisição
+     * Criar novo usuario 
+     * com os dados fornecidos na requisição
      * 
      *   @param \App\http\Requests\UserRequest $request o objeto de requisição contendo os dados do usuario
      * a ser criado
@@ -82,5 +84,85 @@ class UserController extends Controller
         }
     }
 
-    
+
+    /**
+     * Atualizar os dados 
+     * de um usuario existente com base nos dados fornecido na requisição
+     * 
+     *   @param \App\Http\Requests\UserRequest  $request O objeto de requisição contendo os dados do usuario
+     * a ser atualizado.
+     * 
+     *   @param \App\Models\User $user O usuario a ser atualizado
+     *   @return \Illuminate|Http\JsonResponse
+     */
+
+    public function update(UserRequest $request, User $user) : JsonResponse
+    {
+
+        // Iniciar a transação
+        DB::beginTransaction();
+
+        try {
+
+                //Editando os registro do bando 
+            $user->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+
+                //concluir a ediçãp
+            DB::commit();
+
+                //Retornando os dados do usuarios editando e retornando uma msg de sucesso com status 200
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => 'Usuario editado com sucesso'
+            ], 200);
+
+        } catch (Expectation $e) {
+
+                //Nao confirma a operação
+            DB::rollBack();
+                //Retorna a msg de erro 400
+            return response()->json([
+                'status' => false,
+                'message' => 'Usuario nao editado!'
+            ], 400);
+        }
+
+    }
+
+
+    /**
+     * Excluir usuario 
+     * do banco de dados
+     * 
+     *  @param \App\Models\User  $user O usuario a ser excluido
+     *  @return \Illuminate\Http\JsonResponse
+     */
+
+    public function destroy(User $user) : JsonResponse
+    {
+        try {
+
+                //apagar o registro do banco de dado
+            $user->delete();
+
+                //Retornando os dados do usuario apagado e a uma msg de sucesso com o status 200
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => 'Usuario deletado com sucesso'
+            ], 200);
+
+        } catch (Expectation $e) {
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Erro ao deletar usuario'
+            ], 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ Route::get('/users', [UserController::class, 'index']); // GET - http://127.0.0.
 Route::get('/users/{user}', [UserController::class, 'show']); // GET - http:127.0.1:8000/api/users/1
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
